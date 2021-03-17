b856d292759750870a29a4018c1f311553a3b807
Ronaldo Ottoni Borges de Carvalho <ronaldoottoni@gmail.com>
2021-03-17 12:03:31 -0300

    Adicionar arquivos de projeto.

---
+++ ToDoList/Controllers/ToDoesController.cs
@@ -0,0 +1,226 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class ToDoesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: ToDoes
+        
+        //Obriga o usuário a estar logado para ver a lista de tarefas
+        //[Authorize]
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Funcao que busca a lista de todo para o usuario logado
+        private IEnumerable<ToDo> GetMyToDoes()
+        {
+            //Identificar o usuário logado
+            string currentUserId = User.Identity.GetUserId();
+            //Database Query para identificar o usuário 
+            ApplicationUser currentUser = db.Users.FirstOrDefault
+                (x => x.Id == currentUserId);
+
+            IEnumerable < ToDo > myToDoes = db.ToDos.ToList().Where(x => x.User == currentUser);
+
+            int completeCount = 0;
+            foreach (ToDo toDo in myToDoes)
+            {
+                if (toDo.IsDone)
+                {
+                    completeCount++;
+                }
+            }
+
+            ViewBag.Percent = Math.Round(100f * ((float)completeCount / (float)myToDoes.Count()));
+
+            return myToDoes;
+        }
+
+        public ActionResult BuildToDoTable()
+        {
+            //Retorna apenas as tarefas especificas do Usuario logado (usuário atualmente logado)
+            return PartialView("_ToDoTable", GetMyToDoes());
+        }
+
+        // GET: ToDoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ToDo toDo = db.ToDos.Find(id);
+            if (toDo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(toDo);
+        }
+
+        // GET: ToDoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ToDoes/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Data,IsDone")] ToDo toDo)
+        {
+            if (ModelState.IsValid)
+            {
+                string currentUserId = User.Identity.GetUserId();
+                ApplicationUser currentUser = db.Users.FirstOrDefault
+                    (x => x.Id == currentUserId);
+
+                toDo.User = currentUser;
+
+                db.ToDos.Add(toDo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(toDo);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AJAXCreate([Bind(Include = "Id,Nome,Data")] ToDo toDo)
+        {
+            if (ModelState.IsValid)
+            {
+                string currentUserId = User.Identity.GetUserId();
+                ApplicationUser currentUser = db.Users.FirstOrDefault
+                    (x => x.Id == currentUserId);
+
+                toDo.User = currentUser;
+                toDo.IsDone = false;
+                toDo.Data = DateTime.Now;
+
+                db.ToDos.Add(toDo);
+                db.SaveChanges();
+                
+            }
+
+            return PartialView("_ToDoTable", GetMyToDoes());
+        }
+
+        // GET: ToDoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            ToDo toDo = db.ToDos.Find(id);
+
+            if (toDo == null)
+            {
+                return HttpNotFound();
+            }
+
+            String currentUserId = User.Identity.GetUserId();
+            ApplicationUser currentUser = db.Users.FirstOrDefault
+                (x => x.Id == currentUserId);
+
+            if (toDo.User != currentUser)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            return View(toDo);
+        }
+
+        // POST: ToDoes/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome,Data,IsDone")] ToDo toDo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(toDo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(toDo);
+        }
+
+        [HttpPost]
+        public ActionResult AJAXEdit(int? id, bool value)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            ToDo toDo = db.ToDos.Find(id);
+
+            if (toDo == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                toDo.IsDone = value;
+                db.Entry(toDo).State = EntityState.Modified;
+                db.SaveChanges();
+                return PartialView("_ToDoTable", GetMyToDoes());
+            }
+        }
+
+        // GET: ToDoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ToDo toDo = db.ToDos.Find(id);
+            if (toDo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(toDo);
+        }
+
+        // POST: ToDoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ToDo toDo = db.ToDos.Find(id);
+            db.ToDos.Remove(toDo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
